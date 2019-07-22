ab551be407d27bd01074015809d26bc0c3bed092
Rodgers Kishagha <mkishagha@gmail.com>
2019-07-22 10:56:51 +0300

    move babel configuration to babel.config

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,6 @@ module.exports = {
         test: /\.jsx?/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env'],
-        },
       },
       {
         test: /\.css$/,
