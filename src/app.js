1c2e26bc7922bb45db3e091b0a5861180f8c5e02
Rodgers Kishagha <mkishagha@gmail.com>
2019-08-02 12:13:02 +0300

    add dummy emails

--- src/app.js
+++ src/app.js
@@ -5,6 +5,33 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+app.get('/emails', (req, res) => {
+  const emails = [
+    {
+      id: '1',
+      subject: 'My first subject',
+      body:
+        'This is just a dummy emai, open it then we will get to real business later. Regards',
+      timeStamp: Date.now() + 1003,
+    },
+    {
+      id: '2',
+      subject: 'My second subject',
+      body:
+        'This is just a dummy emai, open it then we will get to real business later. Regards',
+      timeStamp: Date.now() + 2004,
+    },
+    {
+      id: '3',
+      subject: 'My third subject',
+      body:
+        'This is just a dummy emai, open it then we will get to real business later. Regards',
+      timeStamp: Date.now() + 3005,
+    },
+  ];
+  return res.json(emails);
+});
+
 app.all('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
